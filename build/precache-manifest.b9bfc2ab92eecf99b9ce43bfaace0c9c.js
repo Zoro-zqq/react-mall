97a29b5958e6f1c9d5ea0c5ed5fdb31cd8b6214e
zzq-zoro <1422345431@qq.com>
2021-01-20 06:38:31 +0800

    react mall

---
+++ build/precache-manifest.b9bfc2ab92eecf99b9ce43bfaace0c9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af165925d23022a30a0c",
+    "url": "/static/js/runtime~main.af165925.js"
+  },
+  {
+    "revision": "3b90c7efe10d8449d9d8",
+    "url": "/static/js/main.3b90c7ef.chunk.js"
+  },
+  {
+    "revision": "21be23b297ea0cc52e4a",
+    "url": "/static/js/2.21be23b2.chunk.js"
+  },
+  {
+    "revision": "f0892cca9ca7df395795",
+    "url": "/static/js/1.f0892cca.chunk.js"
+  },
+  {
+    "revision": "f0892cca9ca7df395795",
+    "url": "/static/css/1.45469121.chunk.css"
+  },
+  {
+    "revision": "8d4a05cae515b67ab47d52c785163ba7",
+    "url": "/index.html"
+  }
+];
---
+++ src/pages/user/myorder/add_review.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {connect} from "react-redux";
+import {safeAuth} from '../../../assets/js/utils/util.js';
+import SubHeaderComponent from '../../../components/header/subheader';
+import Css from '../../../assets/css/user/myorder/add_review.css';
+class  AddReview extends React.Component{
+    constructor(props){
+        super(props);
+        safeAuth(props);
+        this.state = {
+
+        }
+    }
+    componentDidMount(){
+
+    }
+    componentWillUnmount(){
+        this.setState=(state,callback)=>{
+            return;
+        }
+    }
+    render(){
+        return(
+            <div className={Css['page']}>
+                <SubHeaderComponent title="评价"></SubHeaderComponent>
+                <div className={Css['main']}>
+                    <ul className={Css['service']}>
+                        <li>整体</li>
+                        <li>
+                            <div className={Css['stars']+" "+Css['active']}></div>
+                            <div className={Css['stars']}></div>
+                            <div className={Css['stars']}></div>
+                            <div className={Css['stars']}></div>
+                            <div className={Css['stars']}></div>
+                        </li>
+                    </ul>
+                    <ul className={Css['service']}>
+                        <li>服务</li>
+                        <li>
+                            <div className={Css['stars']+" "+Css['active']}></div>
+                            <div className={Css['stars']}></div>
+                            <div className={Css['stars']}></div>
+                            <div className={Css['stars']}></div>
+                            <div className={Css['stars']}></div>
+                        </li>
+                    </ul>
+                    <div className={Css['content-wrap']}>
+                        <textarea placeholder="来分享你的消费感受吧!"></textarea>
+                    </div>
+                    <button className={Css['submit']} type="button">提交</button>
+                </div>
+            </div>
+        );
+    }
+}
+export default connect((state)=>{
+    return{
+        state:state
+    }
+})(AddReview)
